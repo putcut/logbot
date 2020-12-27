1e28b20b660b815e42d1def0ebc7ad6596bfd493
putcut <putcutpoint@gmail.com>
2020-12-27 11:08:36 +0900

    Add start stop commnet.

--- logbot.py
+++ logbot.py
@@ -26,10 +26,12 @@ observer.schedule(event_handler, config.DIR_PATH, recursive=True)
 @bot.command()
 async def start(ctx):
   observer.start()
+  ctx.send("Start log observer.")
 
 @bot.command()
 async def stop(ctx):
   observer.stop()
+  ctx.send("Stop log observer.")
 
 async def send_message(text):
   await bot.guilds[0].text_channels[0].send(text)
